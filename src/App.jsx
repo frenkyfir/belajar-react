6b0f0e6ee008260ff014c28524543a5cbfde43e3
frenkyfir <frenkyfir@gmail.com>
2023-12-20 21:16:11 +0700

    add components 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 const Button = (props) =>{
+  const {children="....",color="bg-black"}=props;
   return(
-  <button className={`h-10 px-6 font-semibold rounded-md ${props.color} text-white`} 
+    
+  <button className={`h-10 px-6 font-semibold rounded-md ${color} text-white`} 
         type="submit">
-          halo
+          {children}
         </button>
   );
 };
@@ -13,8 +15,9 @@ function App() {
   return (
     <div className="flex justify-center bg-blue-600 min-h-screen items-center">
       <div className="flex gap-x-3">
-        <Button color="bg-red-700"></Button>
-        <Button color="bg-slate-700"></Button>
+        <Button color="bg-red-700">Halo</Button>
+        <Button color="bg-red-700">dek</Button>
+        <Button ></Button>
         </div>
     </div>
   );
