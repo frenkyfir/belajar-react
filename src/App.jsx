336f55d58ba9efcbeb1b7e5ec87f18a0c1929ea5
frenkyfir <frenkyfir@gmail.com>
2023-12-20 21:16:15 +0700

    add components 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
 import React from "react";
-const Button = (props) =>{
-  const {children="....",color="bg-black"}=props;
-  return(
-    
-  <button className={`h-10 px-6 font-semibold rounded-md ${color} text-white`} 
-        type="submit">
-          {children}
-        </button>
-  );
-};
+import Button from "./components/Elements/Button"
+import InputForm from "./components/Elements/Input";
 
 function App() {
 
   return (
-    <div className="flex justify-center bg-blue-600 min-h-screen items-center">
-      <div className="flex gap-x-3">
-        <Button color="bg-red-700">Halo</Button>
-        <Button color="bg-red-700">dek</Button>
-        <Button ></Button>
-        </div>
+    <div className="flex justify-center min-h-screen items-center">
+      <div className="w-full max-w-xs">
+        <h1 className="text-3xl font-bold mb-2 text-blue-600">
+          login
+        </h1>
+        <p className="font-medium text-slate-500 mb-8">
+          Welcome, Please enter ur credentials
+        </p>
+        <form action="">
+         <InputForm label="email" type="email" placeholder="halo@mail.com" name="email"/>
+          <div className="mb-6">
+            <label htmlFor="password" className="block text-slate-700 text-sm font-bold mb-2">
+              Password
+            </label>
+            <input type="password" className="text-sm border rounded w-full py-2 px-3 text-slate-700 placeholder: opacity-50" placeholder="********"/>
+          </div>
+          <Button classname="bg-blue-600 w-full">Login</Button>
+        </form>
+      </div>
     </div>
   );
 }
